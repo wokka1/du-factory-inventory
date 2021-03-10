b8d1937d8d9275ab77e690cd40644bc53470677c
Joe Rogers <1337joe@gmail.com>
2021-03-10 22:20:28 +0100

    add comissioned template for Honest Vif's (utopia station)

---
+++ display/screen/honest-vifs.row1.lua
@@ -0,0 +1,72 @@
+----------------------------------------
+-- Factory Inventory: Display Manager --
+--        By W3asel (1337joe)         --
+----------------------------------------
+-- This display commissioned by Vifrevaert for Honest Vif's store at Utopia Station
+-- Bundled: ${date}
+-- Latest version always available here: https://du.w3asel.com/du-factory-inventory/templates/template.display.honest-vifs.json
+
+local screenConfig = {
+    vertical = true,
+    titleFontSize = 75,
+    titleHeight = 100,
+    headerRuleHeight = 5,
+    fontSize = 35,
+    xPadding = 20,
+    tableXPadding = 10,
+    rowHeight = 46.5,
+    rowPadding = 10,
+    countOffset = -100,
+    columns = 1,
+    colspan = 1,
+    tables = {}
+}
+screenConfig.tables[#screenConfig.tables + 1] = {
+    title = "Row 1",
+    columns = {"Top Left", "Top Right"},
+    rows = {
+        {{name = "Corner Cable Model C"},{name = "Resurrection Node"}},
+        {{name = "Cable Model C S"},{name = "Steel Panel"}},
+        {{name = "Cable Model B M"},{name = "Shelf Half Full"}},
+        {{name = "Corner Cable Model A"},{name = "Wingtip L"}},
+        {{name = "Cable Model A S"},{name = "Wingtip S"}},
+        {{name = "Screen M"},{name = "Hull Decorative Element B", label = "Hull Dec B"}},
+        {{name = "Screen XS"},{name = "Force Field L"}},
+        {{name = "Relay"},{name = "Force Field S"}},
+        {{name = "Detection Zone M"},{name = "Headlight"}},
+        {{name = "Detection Zone XS"},{name = "Long Light M"}},
+        {{name = "Squash Plant Case"},{name = "Long Light XS"}},
+        {{name = "Plant Case A"},{name = "Square Light M"}},
+        {{name = "Ficus Plant A"},{name = "Square Light XS"}},
+        {{name = "Plant"},{name = "Vertical Light M"}},
+        {{name = "Suspended Fruit Plant"},{name = "Vertical Light XS"}},
+    }
+}
+screenConfig.tables[#screenConfig.tables + 1] = {
+    columns = {"Bottom Left", "Bottom Right"},
+    rows = {
+        {{name = "Steel Column"},{name = "Fuel Intake XS"}},
+        {{name = "Cable Model C M"},{name = "Shelf Full"}},
+        {{name = "Corner Cable Model B"},{name = "Shelf Empty"}},
+        {{name = "Cable Model B S"},{name = "Wingtip M"}},
+        {{name = "Cable Model A M"},{name = "Vertical Wing"}},
+        {{name = "Transparent Screen L"},{name = "Hull Decorative Element C", label = "Hull Dec C"}},
+        {{name = "Screen S"},{name = "Hull Decorative Element A", label = "Hull Dec A"}},
+        {{name = "Programming Board"},{name = "Force Field M"}},
+        {{name = "Detection Zone L"},{name = "Force Field XS"}},
+        {{name = "Detection Zone S"},{name = "Long Light L"}},
+        {{name = "Eggplant Plant Case"},{name = "Long Light S"}},
+        {{name = "Plant Case S"},{name = "Square Light L"}},
+        {{name = "Salad Plant Case"},{name = "Square Light S"}},
+        {{name = "Bagged Plant A"},{name = "Vertical Light L"}},
+        {{name = "Suspended Plant A"},{name = "Vertical Light S"}},
+    }
+}
+
+-- ensure display array exists
+if not _G.displays then
+    _G.displays = {}
+end
+
+-- must link by slot name
+_G.displays[${slotName}] = screenConfig
---
+++ display/screen/honest-vifs.row2.lua
@@ -0,0 +1,72 @@
+----------------------------------------
+-- Factory Inventory: Display Manager --
+--        By W3asel (1337joe)         --
+----------------------------------------
+-- This display commissioned by Vifrevaert for Honest Vif's store at Utopia Station
+-- Bundled: ${date}
+-- Latest version always available here: https://du.w3asel.com/du-factory-inventory/templates/template.display.honest-vifs.json
+
+local screenConfig = {
+    vertical = true,
+    titleFontSize = 75,
+    titleHeight = 100,
+    headerRuleHeight = 5,
+    fontSize = 35,
+    xPadding = 20,
+    tableXPadding = 10,
+    rowHeight = 46.5,
+    rowPadding = 10,
+    countOffset = -100,
+    columns = 1,
+    colspan = 1,
+    tables = {}
+}
+screenConfig.tables[#screenConfig.tables + 1] = {
+    title = "Row 2",
+    columns = {"Top Left", "Top Right"},
+    rows = {
+        {{name = "Window XS"},{name = "Window M"}},
+        {{name = "Surrogate VR Station"},{name = "Armored Window XS"}},
+        {{name = "Trash Can"},{name = "Armored Window M"}},
+        {{name = "Barrier M"},{name = "Dispenser"}},
+        {{name = "Wooden Low Table"},{name = "Square Carpet"}},
+        {{name = "Wooden Dresser"},{name = "Glass Panel M"}},
+        {{name = "Wooden Chair"},{name = "Keyboard Unit"}},
+        {{name = "Navigator Chair"},{name = "Spaceship Hologram S"}},
+        {{name = "Toilet Unit B"},{name = "Command Seat Controller", label = "Command Seat"}},
+        {{name = "Sink Unit"},{name = "Wooden Table L"}},
+        {{name = "Nightstand"}},
+        {{name = "Bed"}},
+        {{name = "Sofa"}},
+        {{name = "Wooden Armchair"}},
+        {{name = "Wooden Table M"}},
+    }
+}
+screenConfig.tables[#screenConfig.tables + 1] = {
+    columns = {"Bottom Left", "Bottom Right"},
+    rows = {
+        {{name = "Surrogate Pod Station"},{name = "Window S"}},
+        {{name = "Elevator XS"},{name = "Window L"}},
+        {{name = "Barrier Corner"},{name = "Armored Window S"}},
+        {{name = "Barrier S"},{name = "Armored Window L"}},
+        {{name = "Dresser"},{name = "Hatch S"}},
+        {{name = "Encampment Chair"},{name = "Glass Panel S"}},
+        {{name = "Office Chair"},{name = "Glass Panel L"}},
+        {{name = "Urinal Unit"},{name = "Planet Hologram"}},
+        {{name = "Toilet Unit A"},{name = "Spaceship Hologram M"}},
+        {{name = "Shower Unit"},{name = "Hovercraft Seat Controller", label = "Hovercraft Seat"}},
+        {{name = "Wardrobe"}},
+        {{name = "Bench"}},
+        {{name = "Wooden Sofa"}},
+        {{name = "Table"}},
+        {{name = "Wooden Wardrobe"}},
+    }
+}
+
+-- ensure display array exists
+if not _G.displays then
+    _G.displays = {}
+end
+
+-- must link by slot name
+_G.displays[${slotName}] = screenConfig
